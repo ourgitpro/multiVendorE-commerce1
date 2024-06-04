13dd7acb479cfd06e17d0532d1ddbcde5121dcda
Md. Nahid Hossain Eblo <56239953+ourgitpro@users.noreply.github.com>
2024-06-04 07:41:02 -0700

    adminLogin file create

--- src/router/routes/publicRoutes.js
+++ src/router/routes/publicRoutes.js
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 const Login = lazy(() => import("../../views/auth/Login"));
 const Register = lazy(() => import("../../views/auth/Register"));
+const AdminLogin = lazy(()=> import('../../views/auth/AdminLogin')) 
 //0import Login from "../../views/auth/login"
 //import Register from "../../views/auth/register";
 //import Register from './../../views/auth/Register';
@@ -14,5 +15,9 @@ const publicRoutes = [
     path: "/register",
     element: <Register />,
   },
+  {
+    path : '/admin/login',
+    element : <AdminLogin/>
+}
 ];
 export default publicRoutes;
